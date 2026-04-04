f87dd3abe90a579f5aa045ee761368346cdd48ca
unknown <freezy.off@gmail.com>
2026-04-05 06:17:33 +0700

    fresh express for vercel test

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import express, {type Request, type Response} from 'express';
+
+const app = express();
+const PORT: number = 3000;
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('TypeScript with Express');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
